ef6e4e2839c918d547b239c4462b63adee288fd3
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-07-19 22:18:52 +0000

    Index visit table based on animal_id

--- queries.sql
+++ queries.sql
@@ -1,1 +1,5 @@
-/*all queries*/
+--create index on visit table where animal_id is sorted in ascending order
+CREATE INDEX visits_asc ON visits(animal_id ASC);
+
+--run query...execution time has reduced significantly
+SELECT COUNT(*) FROM visits where animal_id = 4;
